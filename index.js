c7a9d32eade204e37b01b43b52c428828b816eae
jigmesherab26 <jigmesherab2026@gmail.com>
2025-11-24 14:59:30 +0100

    removed commented code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const countEl = document.getElementById("count-el")
 const saveEl = document.getElementById("save-el")
 const incrementBtn = document.getElementById("increment-btn")
-// const saveBtn = document.getElementById("save-btn")
 let count = 0
 
 incrementBtn.addEventListener("click", function(){
